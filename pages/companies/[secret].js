cca5055c9dd0fb145dbe214e96fbb25d44c0fb74
Odysseas Sofikitis <o.sofikitis@gmail.com>
2024-01-07 00:08:07 +0200

    Implements companies page

--- pages/companies/[secret].js
+++ pages/companies/[secret].js
@@ -1,30 +1,54 @@
 import Head from 'next/head'
 import Error from 'next/error'
-import { useRouter } from 'next/router'
 import React from 'react'
+import { useState } from 'react'
+import db from '@/utils/db'
 
 import NavigationBar from '@/components/NavigationBar'
+import CompaniesContainer from '@/components/CompaniesContainer'
+import SearchCompanies from '@/components/SearchCompanies'
 
 export const getServerSideProps = async ({ params }) => {
   const { secret } = params
 
   if (secret == process.env.ADMIN_SECRET) {
-    return { props: { verified: true, secret: secret } }
+    const [rows] = await db.execute('SELECT * FROM company')
+    return { props: { verified: true, secret: secret, data: rows } }
   }
 
   return { props: { verified: false } }
 }
 
-export default function Companies({ verified, secret }) {
+export default function Professionals({ verified, secret, data }) {
   if (!verified) return (<Error statusCode={401} />)
+
+  const [filteredData, setFilteredData] = useState(data)
+
+  const handleSearch = ({ name, startedBefore, startedAfter }) => {
+    const filtered = data.filter((company) => {
+      const nameMatch = !name || company.name.toLowerCase().includes(name.toLowerCase())
+
+      const startedBeforeMatch = !startedBefore || (company.yearEST && company.yearEST <= startedBefore)
+
+      const startedAfterMatch = !startedAfter || (company.yearEST && company.yearEST >= startedAfter)
+
+      return nameMatch && startedBeforeMatch && startedAfterMatch
+    })
+
+    setFilteredData(filtered)
+  }
+
+
 
   return (
     <div className='containerNotB'>
       <Head>
-        <title>Companies</title>
+        <title>Professionals</title>
       </Head>
 
-      <NavigationBar secret={secret}/>
+      <NavigationBar secret={secret} />
+      <SearchCompanies onSearch={handleSearch} />
+      <CompaniesContainer companies={filteredData} />
     </div>
   )
 }
